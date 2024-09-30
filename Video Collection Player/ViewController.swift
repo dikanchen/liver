5fc543a252d3e5dd622905a0b98f46a2b7c4bc7c
Dikan Chen <dikan@storyboard.fm>
2024-09-30 13:16:42 -0700

    Initial Commit

---
+++ Video Collection Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video Collection Player
+//
+//  Created by Dikan Chen on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Video Collection PlayerTests/Video_Collection_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Collection_PlayerTests.swift
+//  Video Collection PlayerTests
+//
+//  Created by Dikan Chen on 9/30/24.
+//
+
+import Testing
+@testable import Video_Collection_Player
+
+struct Video_Collection_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
